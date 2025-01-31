a6dd74f44dc56205dd6cd16505570313fdc49164
LautaroN21 <lautaronavarrete6@gmail.com>
2025-01-31 23:10:00 +0100

    se agrega firebase y firestore al proyecto

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Category from './components/Category';
 import ProductDetail from './components/ProductDetail';
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { firebaseApp } from ".";
 
 const App = () => (
   <Router>
@@ -15,5 +18,27 @@ const App = () => (
     </Routes>
   </Router>
 );
+
+function App() {
+const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const db = getFirestore(firebaseApp);
+
+    const biciRef = doc(db, "items", "x5pJaob5CEIseOhQom28");
+  
+    getDoc(biciRef).then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.id, doc.data());
+        setProduct({id: snapshot.id, ...snapshot.data() });
+        console.log({id: snapshot.id, ...snapshot.data()});
+      } else {
+        console.log("Item no encontrado");
+      }
+    });
+   }, []),
+
+    return <div className="App"></div>;
+  }
 
 export default App;
--- ecommerce-app/src/main.jsx
+++ ecommerce-app/src/main.jsx
@@ -1,7 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
 import App from './App.jsx'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDVaxzoSoDvDcJQWViSkfWJLM4FQEIIvHs",
+  authDomain: "proyecto-final-85223.firebaseapp.com",
+  projectId: "proyecto-final-85223",
+  storageBucket: "proyecto-final-85223.firebasestorage.app",
+  messagingSenderId: "702656010724",
+  appId: "1:702656010724:web:3debb90cabbb4389630ff1",
+  measurementId: "G-9X2Y6594TP"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
